e0ac9c2cca80e641d35adebe0d5d129da547cbbf
Fixya <114008002+Fixya@users.noreply.github.com>
2022-10-08 18:28:47 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	//setlocale(LC_ALL, "Russian");
+	//1
+	/*int hp;
+	cin >> hp;
+	bool res;
+	res = hp > 0;
+	cout << boolalpha << res << endl;*/
+	
+	//2
+	/*int k;
+	cin >> k;
+	if (k > 20) {
+		cout << "on";
+	}
+	else {
+		cout << "off";
+	}*/
+
+	//3
+	/*int k, n, pr;
+	cin >> k >> n >> pr;
+	if (k > n && pr < 80) {
+		cout << "on";
+	}
+	else {
+		cout << "off";
+	}*/
+
+	//4
+	/*double x, y;
+	cin >> x;
+	if (x > 0) {
+		y = sqrt((double)x - 2);
+	}
+	else {
+		if (x == 0) {
+			y = 0;
+		}else{
+			if (x < 0) {
+				y = abs(x);
+			}
+		}
+	}
+	cout << y;*/
+
+	//5
+	/*int a;
+	cin >> a;
+	switch (a) {
+	case 1:
+		cout << "большой палец"; break;
+	case 2:
+		cout << "указательный палец"; break;
+	case 3:
+		cout << "средний палец"; break;
+	case 4:
+		cout << "безыянный палец"; break;
+	case 5:
+		cout << "мезинец палец"; break;
+	default:
+		cout << "ошибка"; break;
+	}
+	return 0;*/
+
+	//6
+	/*int a, b;
+	cin >> a >> b;
+	if(a > 20 && a <= b){
+		cout << "ac on";
+	}
+	else{
+		if(a < 20 && a <= b){
+			cout << "ac off";
+		}
+		else {
+			if (a > b){
+				cout << "open window";
+			}
+		}
+	}*/
+}
